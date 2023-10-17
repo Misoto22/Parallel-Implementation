e0ecde1ecd1f4c588dba611c7d7aa08107cdeb63
Henry Chen <cxw2234466599@gmail.com>
2023-10-17 19:37:25 +0800

    first tetsing

--- submit.sh
+++ submit.sh
@@ -3,12 +3,28 @@
 #SBATCH --partition=debug
 #SBATCH --ntasks=2
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=00:01:00
+#SBATCH --time=00:10:00
 #SBATCH --exclusive
 #SBATCH --mem-per-cpu=32G
-#module load  openmpi/4.0.5
 
+echo "=== Running: No OpenMP, No MPI ==="
+gcc -o compile/FSB2_sequential FSB2.c -lm
+srun -n 1 ./compile/FSB2_sequential
+echo "=================================="
+
+echo "=== Running: OpenMP, No MPI ==="
 export OMP_NUM_THREADS=2
-gcc -o FSB2_sequential FSB2.c -lm
-mpicc -fopenmp -o FSB2 FSB2.c
-srun ./FSB2
+gcc -fopenmp -o compile/FSB2_OpenMP FSB2.c -lm
+srun -n 1 ./compile/FSB2_OpenMP
+echo "=============================="
+
+echo "=== Running: No OpenMP, MPI ==="
+#module load openmpi/4.0.5  # Uncomment if needed
+mpicc -D USE_MPI -o compile/FSB2_MPI FSB2.c -lm
+srun -n 2 ./compile/FSB2_MPI
+echo "============================="
+
+echo "=== Running: OpenMP and MPI ==="
+mpicc -D USE_MPI -fopenmp -o compile/FSB2_OpenMP_MPI FSB2.c -lm
+srun -n 2 ./compile/FSB2_OpenMP_MPI
+echo "==============================="
