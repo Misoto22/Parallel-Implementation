e773ae498a1a2f157f26cd2a777c121fa6d47b9c
Henry Chen <cxw2234466599@gmail.com>
2023-10-18 16:54:08 +0800

    OMP_MPI_results.csv

--- submit.sh
+++ submit.sh
@@ -1,41 +1,37 @@
 #!/bin/sh
 #SBATCH --account=courses0101
 #SBATCH --partition=debug
-#SBATCH --ntasks=4
-#SBATCH --ntasks-per-node=1
 #SBATCH --time=00:10:00
 #SBATCH --exclusive
 #SBATCH --mem-per-cpu=32G
 
-#SBATCH --output=Testing\ Results/slurm_output/slurm-%j.out
+#SBATCH --output="Testing Results/slurm_output/slurm-%j.out"
 
 # Define Variables
-test_file="Testing Results/results.csv"
+test_file="Testing Results/OMP_MPI_results.csv"
 compiler_flags="-lm"
 
 # Create results file if it doesn't exist
 if [ ! -f "$test_file" ]; then
-    echo "MPI, OpenMP, Schedule Type, Chunk Size, Barycenter, Elapsed time(s), Number of processes, Number of threads, Iterate times, Number of fishes" >"$test_file"
+    echo "MPI Nodes, OpenMP Threads, Elapsed time(s), Number of fishes" > "$test_file"
 fi
 
 # Function to run tests
 run_test() {
     echo "=== Running: $1 ==="
-    $2                  # Compilation command
-    srun -n 4 -c 4 "$3" # Run command
+    $2  # Compilation command
+
+    # Vary the number of MPI nodes
+    for mpi_nodes in 2 3 4; do
+        # Vary the number of OpenMP threads
+        for omp_threads in 2 4 8 16; do
+            export OMP_NUM_THREADS=$omp_threads
+            srun -n $mpi_nodes -c $omp_threads "$3"  # Run command
+        done
+    done
+
     echo "====================="
 }
 
-# Test 1: No OpenMP, No MPI
-run_test "No OpenMP, No MPI" "gcc -o compile/FSB2_sequential FSB2.c $compiler_flags" "./compile/FSB2_sequential"
-
-# Test 2: OpenMP, No MPI
-export OMP_NUM_THREADS=16
-export OMP_SCHEDULE=static
-run_test "OpenMP, No MPI" "gcc -fopenmp -o compile/FSB2_OpenMP FSB2.c $compiler_flags" "./compile/FSB2_OpenMP"
-
-# Test 3: No OpenMP, MPI
-run_test "No OpenMP, MPI" "mpicc -D USE_MPI -o compile/FSB2_MPI FSB2.c $compiler_flags" "./compile/FSB2_MPI"
-
-# Test 4: OpenMP and MPI
+# Compile and run the combined OpenMP and MPI version
 run_test "OpenMP and MPI" "mpicc -D USE_MPI -fopenmp -o compile/FSB2_OpenMP_MPI FSB2.c $compiler_flags" "./compile/FSB2_OpenMP_MPI"
