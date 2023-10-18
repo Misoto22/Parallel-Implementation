c4145f4c09b4dfc2fe42fa18f9def38867e0e97c
Xinwei Chen <xche0274@uni.sydney.edu.au>
2023-10-18 16:57:04 +0800

    submit.sh updated

--- submit.sh
+++ submit.sh
@@ -13,20 +13,21 @@ compiler_flags="-lm"
 
 # Create results file if it doesn't exist
 if [ ! -f "$test_file" ]; then
-    echo "MPI Nodes, OpenMP Threads, Elapsed time(s), Number of fishes" > "$test_file"
+    echo echo "MPI, OpenMP, Schedule Type, Chunk Size, Barycenter, Elapsed time(s), Number of processes, Number of threads, Iterate times, Number of fishes" >"$test_file"
 fi
 
 # Function to run tests
 run_test() {
     echo "=== Running: $1 ==="
-    $2  # Compilation command
+    $2 # Compilation command
+    export OMP_SCHEDULE=static
 
     # Vary the number of MPI nodes
     for mpi_nodes in 2 3 4; do
         # Vary the number of OpenMP threads
         for omp_threads in 2 4 8 16; do
             export OMP_NUM_THREADS=$omp_threads
-            srun -n $mpi_nodes -c $omp_threads "$3"  # Run command
+            srun -n $mpi_nodes -c $omp_threads "$3" # Run command
         done
     done
 
